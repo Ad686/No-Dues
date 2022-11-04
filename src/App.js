ca4d2df5850475c9d76bceb9183cb524f52f5a6d
Your Name <you@example.com>
2022-11-04 13:07:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import Home from './components/home';
+import AddBook from './components/AddBook';
+import AllBooks from './components/AllBooks';
+import ManageStd from './components/ManageStds';
+import StdAllbook from './components/StdAllbook';
+import IssueReq from './components/issuerequest';
+import BookIssued from './components/BookIssued';
+import BookReq from './components/BookReq';
+import GetReqBook from './components/GetReqBook';
+
+function App() {
+      return (
+            <Router>
+                  <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/addbook" element={<AddBook/>}/>
+                        <Route path="/allbooks" element={<AllBooks/>}/>
+                        <Route path="/managestudents" element={<ManageStd/>}/>
+                        <Route path="/books" element={<StdAllbook/>}/>
+                        <Route path="/issuerequest" element={<IssueReq/>}/>
+                        <Route path="/booksissued" element={<BookIssued/>}/>
+                        <Route path="/bookrequest" element={<BookReq/>}/>
+                        <Route path="/bookrequests" element={<GetReqBook/>}/>
+                  </Routes>
+            </Router>
+      );
+}
+
+export default App;
